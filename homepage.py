5f5783acec098c30cec408fcd05186484402a1b4
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-09-17 15:10:59 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -6,7 +6,7 @@ from image_recognition import app as image_recog_app
 st.set_page_config(page_title='Streamlit Apps | Swastik "Polybit" Biswas - Junior Coders', page_icon='🎯', layout = 'centered', initial_sidebar_state = 'expanded')
 
 PAGES = {
-    "Image Recognition": image_recog_app
+    "Image Recognition (IRM)": image_recog_app
 }
 
 st.sidebar.title('All my Apps!')
