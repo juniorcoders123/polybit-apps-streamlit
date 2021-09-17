597525f0dc4c969effcdc2165903dae5c86c96a9
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-09-17 13:59:42 +0530

    Added page icon

--- homepage.py
+++ homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from image_recognition import app as image_recog_app
 
-st.set_page_config(page_title='Streamlit Apps | Swastik "Polybit" Biswas - Junior Coders', page_icon='🎯' layout = 'centered', initial_sidebar_state = 'expanded')
+st.set_page_config(page_title='Streamlit Apps | Swastik "Polybit" Biswas - Junior Coders', page_icon='🎯', layout = 'centered', initial_sidebar_state = 'expanded')
 
 PAGES = {
     "Image Recognition": image_recog_app
