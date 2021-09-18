8687b7917ad0709c697b53bed8226acff0fabd8e
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-09-18 13:55:51 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 #Initial setting up (to avoid errors)
 st.set_page_config(page_title='Streamlit Apps | Swastik "Polybit" Biswas - Junior Coders', page_icon='🎯', layout = 'centered', initial_sidebar_state = 'expanded')
+st.session_state.can_quote = True
 
 #all my apps
 from image_recognition import app as image_recog_app
@@ -17,6 +18,7 @@ default = int(query_params["activity"][0]) if "activity" in query_params else 0
 
 st.sidebar.title('All my Apps!')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()), index=default)
-st.experimental_set_query_params(activity=list(PAGES).index(selection))
+if selection:
+    st.experimental_set_query_params(activity=list(PAGES).index(selection))
 page = PAGES[selection]
 page.app()
