98674004c6e4440bdf729774b2630ad5ac677fa9
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-09-18 15:28:53 +0530

    Update app.py

--- get_inspired/app.py
+++ get_inspired/app.py
@@ -5,20 +5,22 @@ import json
 def get_quote():
     response = requests.get('http://zenquotes.io/api/random')
     json_data = json.loads(response.text)
-    st.session_state.get_inspired_quote = [json_data[0]['q'], json_data[0]['a']]
+    quote = [json_data[0]['q'], json_data[0]['a']]
+    return quote
 
 def app():
-    st.session_state.get_inspired_quote = ['Loading, please wait momentarily.','The website itself, who else did you expect?']
+    if st.session_state.can_quote == True:
+        st.session_state.quote = get_quote()
+        st.session_state.can_quote = False
     st.markdown(f"""
         # Get Inspired
         #### Brighten your day with quotes from the greatest minds...
         Made by Swastik 'Polybit' Biswas
         ## 
-        ## *" {st.session_state.get_inspired_quote[0]} "*
-        *- {st.session_state.get_inspired_quote[1]}*
+        ## *" {st.session_state.quote[0]} "*
+        *- {st.session_state.quote[1]}*
         ## 
     """)
     action = st.button("I'm still not inspired yet. Hit me with another one!")
     if action:
-        get_quote()
-    get_quote()
+        st.session_state.quote = get_quote()
