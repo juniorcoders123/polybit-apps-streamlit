aacccce03c0e19e1d8c79381431dc8f7637b3268
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-09-18 15:27:05 +0530

    Update app.py

--- get_inspired/app.py
+++ get_inspired/app.py
@@ -6,7 +6,6 @@ def get_quote():
     response = requests.get('http://zenquotes.io/api/random')
     json_data = json.loads(response.text)
     st.session_state.get_inspired_quote = [json_data[0]['q'], json_data[0]['a']]
-    st.stop()
 
 def app():
     st.session_state.get_inspired_quote = ['Loading, please wait momentarily.','The website itself, who else did you expect?']
