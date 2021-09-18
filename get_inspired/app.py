5b2d48e5f66818d016b343fb108aa216d19cbcca
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-09-18 15:18:51 +0530

    Update app.py

--- get_inspired/app.py
+++ get_inspired/app.py
@@ -5,13 +5,10 @@ import json
 def get_quote():
     response = requests.get('http://zenquotes.io/api/random')
     json_data = json.loads(response.text)
-    quote = [json_data[0]['q'], json_data[0]['a']]
-    return quote
+    st.session_state.get_inspired_quote = [json_data[0]['q'], json_data[0]['a']]
+    st.stop()
 
 def app():
-    if st.session_state.get_inspired_can_quote == True:
-        st.session_state.get_inspired_quote = get_quote()
-        st.session_state.get_inspired_can_quote = False
     st.markdown(f"""
         # Get Inspired
         #### Brighten your day with quotes from the greatest minds...
@@ -24,3 +21,4 @@ def app():
     action = st.button("I'm still not inspired yet. Hit me with another one!")
     if action:
         st.session_state.get_inspired_quote = get_quote()
+    get_quote()
