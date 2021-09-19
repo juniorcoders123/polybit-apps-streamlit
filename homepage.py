a25e5524386fec494d584d6c32a69ecfc55ccf10
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-09-19 19:12:23 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from bokeh.models.widgets import Div
 
 #Initial setting up (to avoid errors)
 st.set_page_config(page_title='Streamlit Apps | Swastik "Polybit" Biswas - Junior Coders', page_icon='🎯', layout = 'centered', initial_sidebar_state = 'expanded')
@@ -12,7 +13,7 @@ from tic_tac_toe import app as tic_tac_toe_app
 PAGES = {
     "Image Recognition (IRM)": image_recog_app,
     "Get Inspired": get_insp_app,
-    "Tic Tac Toe Game": tic_tac_toe_app
+    "Tic Tac Toe Game (broken)": tic_tac_toe_app
 }
 
 query_params = st.experimental_get_query_params()
@@ -24,3 +25,16 @@ if selection:
     st.experimental_set_query_params(activity=list(PAGES).index(selection))
 page = PAGES[selection]
 page.app()
+
+visit_techninja_site = st.button("Visit TechNinja's Site")
+if visit_techninja_site:
+    js = "window.open('https://share.streamlit.io/juniorcoders123/techninjaallwebapps/main/homepage.py?activity=0')"  # New tab or window
+    html = '<img src onerror="{}">'.format(js)
+    div = Div(text=html)
+    st.bokeh_chart(div)
+github_site = st.button("Visit GitHub repository")
+if github_site:
+    js = "window.open('https://github.com/juniorcoders123/polybit-apps-streamlit')"  # New tab or window
+    html = '<img src onerror="{}">'.format(js)
+    div = Div(text=html)
+    st.bokeh_chart(div)
