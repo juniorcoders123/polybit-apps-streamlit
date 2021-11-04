c71cc6fc02451fc7e1fc08bab046bea613c9ba9d
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-11-04 18:58:01 +0530

    Add files via upload

---
+++ morse_decoder_encoder/app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+
+# write a function that converts english to morse code
+def morse_encode(text):
+    morse_code = {
+        'A': '.-', 'B': '-...',
+        'C': '-.-.', 'D': '-..', 'E': '.',
+        'F': '..-.', 'G': '--.', 'H': '....',
+        'I': '..', 'J': '.---', 'K': '-.-',
+        'L': '.-..', 'M': '--', 'N': '-.',
+        'O': '---', 'P': '.--.', 'Q': '--.-',
+        'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--',
+        'X': '-..-', 'Y': '-.--', 'Z': '--..',
+        '1': '.----', '2': '..---', '3': '...--',
+        '4': '....-', '5': '.....', '6': '-....',
+        '7': '--...', '8': '---..', '9': '----.',
+        '0': '-----', ', ': '--..--', '.': '.-.-.-',
+        '?': '..--..', '/': '-..-.', '-': '-....-',
+        '(': '-.--.', ')': '-.--.-', ' ': '/'
+    }
+    return ' '.join(morse_code[char] for char in text.upper())
+
+# write a function that converts morse code to english
+def morse_decode(text):
+    morse_code = {
+        '.-': 'A', '-...': 'B',
+        '-.-.': 'C', '-..': 'D', '.': 'E',
+        '..-.': 'F', '--.': 'G', '....': 'H',
+        '..': 'I', '.---': 'J', '-.-': 'K',
+        '.-..': 'L', '--': 'M', '-.': 'N',
+        '---': 'O', '.--.': 'P', '--.-': 'Q',
+        '.-.': 'R', '...': 'S', '-': 'T',
+        '..-': 'U', '...-': 'V', '.--': 'W',
+        '-..-': 'X', '-.--': 'Y', '--..': 'Z',
+        '.----': '1', '..---': '2', '...--': '3',
+        '....-': '4', '.....': '5', '-....': '6',
+        '--...': '7', '---..': '8', '----.': '9',
+        '-----': '0', '--..--': ', ', '.-.-.-': '.',
+        '..--..': '?', '-..-.': '/', '-....-': '-',
+        '-.--.': '(', '-.--.-': ')', '/': ' '
+    }
+    return ''.join(morse_code[char] for char in text.split(' '))
+
+st.markdown('''
+    # Morse Decoder + Encoder
+    ## 
+    ## ''')
+operation = st.selectbox('Select type of operation', ('English to Morse','Morse to English'))
+st.markdown('''
+    ## ''')
+if operation == 'English to Morse':
+    st.write('Enter your English text here')
+    english = st.text_area('')
+    st.markdown('## ')
+    st.write('Morse Code Encoding')
+    st.markdown('## ')
+    st.code(morse_encode(english))
+elif operation == 'Morse to English':
+    st.write('Enter your Morse Code here')
+    morse = st.text_area('')
+    st.markdown('## ')
+    st.write('English Decoding')
+    st.markdown('## ')
+    try:
+        st.code(morse_decode(morse))
+    except:
+        st.error('Please enter valid Morse Code')
