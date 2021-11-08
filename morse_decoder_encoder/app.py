4152f56289a4e403a7ab18abd21351d3fea72e9a
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-11-08 14:31:20 +0530

    Update app.py

--- morse_decoder_encoder/app.py
+++ morse_decoder_encoder/app.py
@@ -75,7 +75,6 @@ def app():
         st.markdown('## ')
         st.markdown('###### Morse Code Encoding')
         output = st.empty()
-        audio = st.empty()
         try:
             output.code(morse_encode(english))
         except:
@@ -87,7 +86,7 @@ def app():
                 audio_file = open('morse_decoder_encoder/audio/output.wav', 'rb')
                 audio_bytes = audio_file.read()
                 st.markdown('###### Morse Code Encoding Audio')
-                audio.audio(audio_bytes, format='audio/mp3', start_time=0)
+                st.audio(audio_bytes, format='audio/wav', start_time=0)
             except:
                 output.error('Please use only alphabet characters (A-Z), numbers (0-9), spaces and special characters (,.?/())...\nAny other unicode characters are either not allowed till now or their morse code translations have not been added yet... ')
     elif operation == 'Morse to English (Decoder)':
