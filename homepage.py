f6caa157cee0db365c6b07b8a18e6a18e23f9078
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2021-11-09 11:00:39 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import extra_streamlit_components as stx
 
 #Initial setting up (to avoid errors)
 st.set_page_config(page_title='Streamlit Apps | Swastik "Polybit" Biswas - Junior Coders', page_icon='🎯', layout = 'centered', initial_sidebar_state = 'expanded')
@@ -10,11 +11,16 @@ from get_inspired import app as get_insp_app
 from tic_tac_toe import app as tic_tac_toe_app
 from morse_decoder_encoder import app as morse_app
 
+#all the cookies for apps
+cookie_manager = stx.CookieManager()
+
+# latest --> oldest
+cookies_ref = ('morse_app_like', 'tic_tac_toe_app_like', 'get_insp_app_like', 'image_recog_app_like')
 PAGES = {
-    "Morse Code Decoder + Encoder 🆕": morse_app,
-    "Tic Tac Toe Game [broken 🛠]": tic_tac_toe_app,
-    "Get Inspired": get_insp_app,
-    "Image Recognition (IRM)": image_recog_app
+    ("⭐ " if cookie_manager.get('morse_app_like') else "") + "Morse Code Decoder + Encoder 🆕": morse_app,
+    ("⭐ " if cookie_manager.get('tic_tac_toe_app_like') else "") + "Tic Tac Toe Game [broken 🛠]": tic_tac_toe_app,
+    ("⭐ " if cookie_manager.get('get_insp_app_like') else "") + "Get Inspired": get_insp_app,
+    ("⭐ " if cookie_manager.get('image_recog_app_like') else "") + "Image Recognition (IRM)": image_recog_app
 }
 
 query_params = st.experimental_get_query_params()
@@ -27,7 +33,19 @@ if selection:
 page = PAGES[selection]
 page.app()
 
-visit_techninja_site = "[Visit TechNinja's Site](https://share.streamlit.io/juniorcoders123/techninjaallwebapps/main/homepage.py?activity=0)"
+like_button_text = ''
+if cookie_manager.get(cookies_ref[list(PAGES).index(selection)]):
+    like_button_text = 'Dislike this current project 💔'
+else:
+    like_button_text = 'Like this current project! ⭐'
+like_button = st.sidebar.button(like_button_text)
+if like_button:
+    if cookie_manager.get(cookies_ref[list(PAGES).index(selection)]):
+        cookie_manager.delete(cookies_ref[list(PAGES).index(selection)])
+    else:
+        cookie_manager.set(cookies_ref[list(PAGES).index(selection)], True)
+
+visit_techninja_site = "[Visit TechNinja's Site](https://share.streamlit.io/juniorcoders123/techninjaallwebapps/main/homepage.py)"
 github_repo = '[Visit the GitHub repository](https://github.com/juniorcoders123/polybit-apps-streamlit)'
 st.sidebar.markdown(visit_techninja_site, unsafe_allow_html=True)
 st.sidebar.markdown(github_repo, unsafe_allow_html=True)
