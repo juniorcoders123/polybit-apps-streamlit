b8731be29bdb86edd18c603420c553e0b27c43f5
juniorcoders123 <83218545+juniorcoders123@users.noreply.github.com>
2022-01-11 10:18:03 +0530

    Add files via upload

--- image_effects/app.py
+++ image_effects/app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import cv2
+import numpy as np
 from PIL import Image, ImageOps, ImageFilter, ImageEnhance
+from io import BytesIO
+import base64
 
 def app():
     st.markdown('''
@@ -13,7 +17,7 @@ def app():
         final_image = original_image
         original_image_holder, final_image_holder = st.columns(2)
         original_image_holder.image(original_image, 'Original Image', use_column_width=True)
-        fx = st.selectbox('Select an image effect...', ['None', 'Invert', 'Grayscale', 'Blur', 'Sharpen', 'Contrast', 'Brightness'])
+        fx = st.selectbox('Select an image effect...', ['None', 'Invert', 'Grayscale', 'Blur', 'Sharpen', 'Contrast', 'Brightness', 'Oil Painting Effect', 'Sketch Effect'])
         if fx == 'None':
             pass
         elif fx == 'Invert':
@@ -36,6 +40,20 @@ def app():
             enhancer = ImageEnhance.Brightness(original_image)
             factor = st.slider('Brightness Factor', 0.1, 3.0, 1.5)
             final_image = enhancer.enhance(factor)
+        elif fx == 'Oil Painting Effect':
+            detail = st.slider('Oil Painting Detail', 1, 10, 5)
+            final_image = cv2.xphoto.oilPainting(np.array(original_image), 11-detail, 1)
+        elif fx == 'Sketch Effect':
+            detail = st.slider('Sketch Detail', 15, 39, 21)
+            gray_image = cv2.cvtColor(np.array(original_image), cv2.COLOR_BGR2GRAY)
+            inverted_gi = 255 - gray_image
+            blurred_img = cv2.GaussianBlur(inverted_gi, (detail, detail), 0)
+            inverted_bi = 255 - blurred_img
+            final_image = cv2.divide(gray_image, inverted_bi, scale=256.0)
         else:
             st.error('Please select a valid image effect from the dropdown...')
         final_image_holder.image(final_image, 'Final Image', use_column_width=True)
+        if fx == 'Oil Painting Effect' or fx == 'Sketch Effect':
+            final_image = Image.fromarray(final_image)
+        final_image.save('image_effects/image.jpg')
+        st.download_button('Download Final Image', 'image_effects/image.jpg')
